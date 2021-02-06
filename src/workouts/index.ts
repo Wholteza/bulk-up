9ed1f5da45150ab1383782516d1dfe05e8017b07
Zackarias Montell <zackarias@montell.se>
2021-02-06 09:20:54 +0100

    Deleted unused testing workout

--- src/workouts/index.ts
+++ src/workouts/index.ts
@@ -3,14 +3,7 @@ import WorkoutOne from "./zm1";
 import WorkoutTwo from "./zm2";
 import WorkoutThree from "./zm3";
 import WorkoutFour from "./zm4";
-import WorkoutFive from "./zm5";
 
-const days: Day[] = [
-  WorkoutOne,
-  WorkoutTwo,
-  WorkoutThree,
-  WorkoutFour,
-  WorkoutFive,
-];
+const days: Day[] = [WorkoutOne, WorkoutTwo, WorkoutThree, WorkoutFour];
 
 export default days;
