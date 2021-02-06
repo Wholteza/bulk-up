6f834d1ce3faf2b38398c09d57d08191a1eb2554
Zackarias Montell <zackarias@montell.se>
2021-02-06 09:03:17 +0100

    Added another workout to test azure hosting

--- src/workouts/index.ts
+++ src/workouts/index.ts
@@ -3,7 +3,14 @@ import WorkoutOne from "./zm1";
 import WorkoutTwo from "./zm2";
 import WorkoutThree from "./zm3";
 import WorkoutFour from "./zm4";
+import WorkoutFive from "./zm5";
 
-const days: Day[] = [WorkoutOne, WorkoutTwo, WorkoutThree, WorkoutFour];
+const days: Day[] = [
+  WorkoutOne,
+  WorkoutTwo,
+  WorkoutThree,
+  WorkoutFour,
+  WorkoutFive,
+];
 
 export default days;
